1429e04b63f1b815b6eb4477e03db4ef75558c81
MD Amdadul Islam <58988171+amdadul3036@users.noreply.github.com>
2020-09-19 06:03:51 -0700

    Without semicolon

--- datatype.py
+++ datatype.py
@@ -1,4 +1,4 @@
-print("My name is Dhrubo");
-print("I am 70 years old");
-print("I like my name Dhrubo");
-print("But I don't like my age 70");
+print("My name is Dhrubo")
+print("I am 70 years old")
+print("I like my name Dhrubo")
+print("But I don't like my age 70")
