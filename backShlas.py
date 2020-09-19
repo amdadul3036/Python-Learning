bed8070a1dfb5c6c61daef76c178375a83e14c3b
MD Amdadul Islam <58988171+amdadul3036@users.noreply.github.com>
2020-09-19 06:24:38 -0700

    Backshlash

---
+++ backShlas.py
@@ -0,0 +1,8 @@
+print("Using\Backshlash");
+# It will just print Using/BackSlash
+
+print("Using \" Backshlash");
+# It will print Using " Backshlash. Because \ represents that things after the shlash will be print as string.
+
+print("Using\nBackshlash");
+# It will print new Line
