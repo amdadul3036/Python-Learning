62c699ffd7684b138b56f710746dc38922397df0
MD Amdadul Islam <58988171+amdadul3036@users.noreply.github.com>
2020-09-20 07:34:58 -0700

    Number game

---
+++ numbers.py
@@ -0,0 +1,34 @@
+# Number
+print(5)
+
+# String
+print("5")
+
+# Reminder
+print(10%3)
+
+# Convert a number into a string
+print(str(10))
+
+# Absolute value
+print(abs(-19))
+
+
+# Power of a number
+print(pow(2,5))
+# It will print the value of 2^5
+
+
+
+# Get the MAXIMUM number or LARGEST number
+print(max(3,75,34,78,97,4,678))
+
+
+# Get the MINIMUM number or SMALLEST number
+print(min(3,75,34,78,97,4,678))
+
+
+# Get the round value of a float number
+print(round(35424.4454))
+print(round(35424.6454))
+
