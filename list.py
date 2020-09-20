dd9c855c0f04667dd6305cb7ca07ec5417b89ccb
MD Amdadul Islam <58988171+amdadul3036@users.noreply.github.com>
2020-09-20 09:17:04 -0700

    List

---
+++ list.py
@@ -0,0 +1,19 @@
+# List
+friends = ["Dhrubo" , "Amdadul" , "Dhruboish" , "Oaliullah" , "MD AMDADUL ISLAM"]
+print(friends)
+
+
+# Find a specific index character 
+print(friends[1])
+
+
+print(friends[2:])
+# It will print all elements of friends list from the index number 2.
+
+print(friends[2:4])
+# It will print from index 2 to before the element of index 4
+
+
+# Update element of a list 
+friends[1] = "Mamun"
+print(friends)
