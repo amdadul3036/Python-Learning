2808e3065e66cde75a0361d8654a24aabf4bb40d
MD Amdadul Islam <58988171+amdadul3036@users.noreply.github.com>
2020-09-21 00:35:54 -0700

    Tuple

---
+++ tuple.py
@@ -0,0 +1,15 @@
+# Tuple is like list ; It's index numbering is exactly like list. But we cannot modify or update a tuple.
+
+coordinate = (4,5)
+# This is a tuple and it use paranthesis () 
+print(coordinate)
+# It prints (4,5)
+print(coordinate[0])
+# It will print 0 index element of the tuple that is 4. 
+
+
+
+# We can add multiple tuple in a list 
+couples = [("Rubi","Rubina") , ("Razzak","Shakira") , ("Shakib Khan","Apu Biswas") , ("Ramon","Isha")]
+print(couples)
+print(couples[1])
