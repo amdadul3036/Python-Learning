5948a35404bfa526cb62b629437cd2ba36fdb3f3
MD Amdadul Islam <58988171+amdadul3036@users.noreply.github.com>
2020-09-21 12:37:23 -0700

    Comparing 3 numbers

---
+++ maximum.py
@@ -0,0 +1,10 @@
+def numbers( num1 , num2 , num3 ):
+    if num1>=num2 & num1>=num3:
+        print(num1)
+    elif num2>=num1 & num2>=num3:
+        print(num2)
+    else:
+        print(num3)
+        
+        
+numbers(34,64,75)
