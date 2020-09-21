bf3e3580183f37bf6e24127e1ab0b437c58fd906
MD Amdadul Islam <58988171+amdadul3036@users.noreply.github.com>
2020-09-21 12:07:11 -0700

    if elif

---
+++ if_statement.py
@@ -0,0 +1,15 @@
+is_male = True
+is_student = False
+if is_male and is_student:
+    print("You are a male and student")
+    
+# else if ke python e elif bole  
+elif is_male and not(is_student):
+    print("You are man but not a student")
+elif is_student and not(is_male):
+    print("You are not a man but a student")
+else:
+    print("You are neither a male nor a student")
+    
+    
+# Python er and operator && or operator || not operator ! erokom na 
