775e48d658dd794c49368b6bc0686aba8dbf3cff
MD Amdadul Islam <58988171+amdadul3036@users.noreply.github.com>
2020-09-21 21:55:31 -0700

    Dictionary in python

---
+++ dictionaries.py
@@ -0,0 +1,19 @@
+monthConverter = {
+    "Jan" : "January",
+    "Feb" : "February",
+    "Mar" : "March",
+    "Apr" : "April",
+    "May" : "May",
+    "Jun" : "June",
+    "Jul" : "July",
+    "Aug" : "August",
+    "Sep" : "September",
+    "Oct" : "October",
+    "Nov" : "November",
+    "Dec" : "December",
+}
+
+print(monthConverter["Jan"])
+
+
+# Here "Jan" ,"Feb" etc. are called key value. Key values should be unique in a dictionary. 
