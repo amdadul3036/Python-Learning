14757284e86409abb16ee40fe1ab6e0ae80b65e9
MD Amdadul Islam <58988171+amdadul3036@users.noreply.github.com>
2020-09-21 22:53:20 -0700

    For loop in python

---
+++ for_loop.py
@@ -0,0 +1,6 @@
+friends = ["Dhrubo" , "Dhruboish" , "Amdadul"]
+for friend in friends:
+    print(friend)
+    
+    
+# That is for eact friend inside friends print friend.
---
+++ for_loop_range.py
@@ -0,0 +1,12 @@
+print("Print to 0 to 9: ")
+for x in range(10):
+    print(x)
+    
+    
+# It will Print all numbers from 0 to 10 excluding 10. 
+
+print("Printing 3 to 9: ")
+for y in range(3,10):
+    print(y)
+    
+# It will print numbers from 3 to 10 excluding 10 
