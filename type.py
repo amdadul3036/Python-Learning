e42ce3410141721d739ad53e8b4d2c691a0c6da6
MD Amdadul Islam <58988171+amdadul3036@users.noreply.github.com>
2020-09-22 23:21:15 -0700

    type() in python

---
+++ type.py
@@ -0,0 +1,18 @@
+# We can know the type of a variable using type() function. 
+
+a = 3
+b = 3.1416
+c = "Dhrubo"
+d = True
+
+print(type(a))
+print(type(b))
+print(type(c))
+print(type(d))
+
+
+# OUTPUT 
+# <class 'int'>
+# <class 'float'>
+# <class 'str'>
+# <class 'bool'>
