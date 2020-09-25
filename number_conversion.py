b6858081e8f7f0c13195237eb2b76a209b7d8386
MD Amdadul Islam <58988171+amdadul3036@users.noreply.github.com>
2020-09-25 08:13:46 -0700

    Number system conversion by python

---
+++ number_conversion.py
@@ -0,0 +1,23 @@
+# Convert decimal to binary , octal , hexadecimal using bin() , oct() , hex() respectively. 
+
+print(bin(25))
+print(oct(25))
+print(hex(25))
+# OUTPUT
+# 0b11001
+# 0o31
+# 0x19
+'''
+In python -
+   Binary starts with "0b"
+   Octal starts with "0o"
+   Hexadecimal starts with "0x"
+'''
+
+# Convert binary , octal , hexadecimal to decimal.
+print(0b01110101)
+# It will print 117
+print(0o34512)
+# It will print 14666
+print(0x34afd)
+# It will print 215805
