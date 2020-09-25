a45398d35b1c95e4a132dbbbcbc96f8bd3fa72d5
MD Amdadul Islam <58988171+amdadul3036@users.noreply.github.com>
2020-09-25 07:42:33 -0700

    write a file by python

--- coursera.py
+++ coursera.py
@@ -1,3 +1,8 @@
+tot = 0 
+for i in [5, 4, 3, 2, 1] :
+    tot = tot + 1
+print(tot)
+# ---------------------------------------------------------------------------------------
 def addtwo(a, b):
     added = a + b
     return a
@@ -46,3 +51,20 @@ elif score>=0.6:
     print("D")
 else:
     print("F")
+    
+    
+    
+    # ----------------------------------------------------------------------------------------
+    
+smallest_so_far = -1
+for the_num in [9, 41, 12, 3, 74, 15] :
+   if the_num < smallest_so_far :
+      smallest_so_far = the_num
+print(smallest_so_far)
+
+# --------------------------------------------------------------------------------------------
+n = 0
+while n > 0 :
+    print('Lather')
+    print('Rinse')
+print('Dry off!')
---
+++ write_files.py
@@ -0,0 +1,6 @@
+
+appending_in_file = open("student.txt", "a")
+
+print(appending_in_file.write("\nArun - 11803056"))
+
+appending_in_file.close()
