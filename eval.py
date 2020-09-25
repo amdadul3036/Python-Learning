5c53c5d7c88b90d902ef9721a2defa92f7dca4ac
MD Amdadul Islam <58988171+amdadul3036@users.noreply.github.com>
2020-09-25 08:47:01 -0700

    User input() in python

---
+++ eval.py
@@ -0,0 +1,4 @@
+# We can evaluate any mathematical function by python. 
+ 
+x = eval(input(2+3+9))
+print(x)
---
+++ user_input.py
@@ -0,0 +1,14 @@
+x = input("Enter your mind talk: ")
+print(x)
+y = input("Enter the value you want : ")
+print(y)
+print(type(y))
+# It will print though we are giving some numeric value input() taking it as string. 
+
+
+# Now how can we convert a string to a number?
+z = float(input("Enter the first number: "))
+print(type(z))
+m = float(input("Enter the second number: "))
+print(type(m))
+print(z + m)
