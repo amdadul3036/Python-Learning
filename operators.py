982080c6975873b02cd6d64c08a56093fef00211
MD Amdadul Islam <58988171+amdadul3036@users.noreply.github.com>
2020-09-25 08:03:39 -0700

    Operators

---
+++ operators.py
@@ -0,0 +1,26 @@
+# Arithmetic Operator 
+#  + , - , * , / 
+print(4+5)
+
+
+
+# Assignment Operator
+#  = 
+
+x = 6
+print(x)
+
+a , b = 1 , 2
+print(a)
+# It will Print 1 . 
+print(b)
+# It will print 2 . 
+
+
+
+# Relational Operator 
+# > , < , <= , >= , == , === , != 
+
+
+# Logical Operator 
+# True , False , and , or , NOT 
