eea5b3134d945467e9bda5e2c4e3703fbc0cd84e
MD Amdadul Islam <58988171+amdadul3036@users.noreply.github.com>
2020-09-26 05:21:30 -0700

    For vs While

--- for_loop.py
+++ for_loop.py
@@ -3,4 +3,6 @@ for friend in friends:
     print(friend)
     
     
-# That is for eact friend inside friends print friend.
+# That is for each friend inside friends print friend.
+# In python only use for loop if you want to work with each element one by one. Otherwise
+# work with while loop.
