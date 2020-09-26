b49fbae0e20d8140176c2771c595848ce433f30d
MD Amdadul Islam <58988171+amdadul3036@users.noreply.github.com>
2020-09-26 08:10:51 -0700

    Continue

---
+++ breake_continue_pass.py
@@ -0,0 +1,17 @@
+for i in range(5):
+    if i == 3:
+        continue
+    print("Hello - $" , i)
+
+'''
+OUTPUT
+Hello - $ 0
+Hello - $ 1
+Hello - $ 2
+Hello - $ 4
+'''
+
+# So continue just skip the section in scilence . Look as we continue i == 3 so it does not print Hello - $ 3 anymore. It prints rest of the other. 
+
+# -----------------------------------------------------------------------------------------------
+
