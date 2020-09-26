13df40f5b90260b2d673112c5629d1de1d329bf0
MD Amdadul Islam <58988171+amdadul3036@users.noreply.github.com>
2020-09-26 06:09:02 -0700

    Print all numbers from 1 to 300 which are not divisible by 3 and 5

---
+++ Project2.py
@@ -0,0 +1,12 @@
+# Print all numbers from 1 to 300 which are not divisible by 3 and 5. 
+for i in range(1,301):
+    if i%3!=0 and i%5!=0:
+        print(i)
+        
+        
+        
+        
+        
+# Important Learning Here:
+#     And operator in python = and
+#     Not Equal Operator in python = "!="
