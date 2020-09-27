4da76bb598439c4d326d381bbe31c75764b82cdd
MD Amdadul Islam <58988171+amdadul3036@users.noreply.github.com>
2020-09-27 07:21:47 -0700

    Array in python

--- array_vs_list.py
+++ array_vs_list.py
@@ -4,6 +4,17 @@ from array import *
 
 val = array( "i" , [2,-4,6,2,8,5])
 print(val)
+print(val.buffer_info())
+
+# Printing all values of an array. 
+for p in val:
+    print(p)
 
 val1 = array( "I" , [2,4,6,2,8,5])
 print(val1)
+
+
+
+# For characters you should use "u". U for unique . 
+val2 = array('u' , ['A' , 'm' , 'd' , 'a' , 'd' , 'u' , 'l'])
+print(val2)
