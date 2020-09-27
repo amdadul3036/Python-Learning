3d26c3a187c09cb2faa9fa88aae9260d6c7488a9
MD Amdadul Islam <58988171+amdadul3036@users.noreply.github.com>
2020-09-27 06:39:54 -0700

    Array in python in wrong way

--- array_vs_list.py
+++ array_vs_list.py
@@ -1,7 +1,6 @@
-# Lists and arrays are used in Python to store data(any data type- strings, integers etc), both can be indexed and iterated also. Difference between lists and arrays are the functions that you can perform on them like for example when you want to divide an array by 4, the result will be printed on request but in case of a list, python will throw an error message. Here's how it works: 
+# In list you can use any data type together but in array you have to use same data type elements. 
 
+from array import *
 
-from numpy import array
-a = array([4,8,16,20])
-a = a/4
-print(a)
+val = array([2,4,6,2,8,5])
+print(val)
