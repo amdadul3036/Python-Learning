4c8175da5354e070ecc99baf00a600a62fe0bb78
MD Amdadul Islam <58988171+amdadul3036@users.noreply.github.com>
2020-09-29 20:30:09 -0700

    String Manipulation in Python

--- function.py
+++ function.py
@@ -2,3 +2,9 @@ def my_fun():
     print('Hello Dhrubo')
 
 my_fun()
+
+
+def add(a,b):
+    print(a + b)
+
+add(10,11)
--- string.py
+++ string.py
@@ -10,6 +10,7 @@ print(phrase.lower())
 # Checking whether it's lowercase or uppercase
 print(phrase.isupper())
 # It will be true if all of the alphabet of phrase are in upperCase
+
 print(phrase.islower())
 # It will be true if all of the alphabet of phrase are in lowerCase
 
@@ -53,3 +54,68 @@ print(phrase[:1000])
 # Replace word or character by another
 print(phrase.replace("DHRUBOISH", "Dhruboish Youtube"))
 # Here 1st string inside the replace() function is the word you want to change and the last one is the word by which you want to change.
+
+
+# Check wither a character is in the phrase or not . 
+print('abc' in phrase)
+# It's false because 'abc' does not have inside Phrase. 
+
+print('Dhrubo' in phrase)
+# It's true because 'Dhrubo' have inside phrase. 
+
+print('Dhrubo' not in phrase)
+# It is false because 'Dhrubo' does have inside the phrase. 
+
+
+
+# Check wither it is numeric or not 
+print(phrase.isnumeric())
+# It if FALSE because phrase is not Numeric
+
+
+# Check wither Phrase starts with the specific word you want
+print(phrase.startswith("abc"))
+# It is FALSE because phrase does not start with the word 'abc'
+
+
+
+print(phrase.startswith("MD"))
+# It is TRUE because phrase starts with the word 'MD'
+
+
+print('Checking startswith Case Sensitivity: ',phrase.startswith('md'))
+# It is FALSE because phrase starts with with 'MD' not with 'md'
+# SO it is CASE SENSITIVE
+
+
+# Similarly We can check the end word 
+print('CHECKING ENDSWITH : ',phrase.endswith('DHRUBOISH'))
+
+
+splitUpSentence = phrase.split(' ')
+print('Using split function to split pharase: ', splitUpSentence)
+# This split function will split phrase whenever it see ' ' an empty string and print all splited characters in an array. 
+
+
+
+# Now we can join splited characters of splitUpSentence by using .join() function 
+joinTheCharacters = ' '.join(splitUpSentence)
+print('Joining all characters of splitUpSentence: ',joinTheCharacters)
+# That is it joining all characters with ' ' this empty string
+
+
+
+
+collage = '         My Collage name is Notre Dame Collage, Dhaka          '
+print('Collage: ',collage)
+print(len(collage))
+
+
+print(collage.strip(' '))
+# strip() will remove all extra spaces from both side.
+
+print(collage.lstrip(' '))
+# lstrip() will remove all extra spaces from left side.
+
+print(collage.rstrip(' '))
+# rstrip() will remove all extra spaces from right side.
