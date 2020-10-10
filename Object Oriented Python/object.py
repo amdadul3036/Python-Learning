d0aaadf6a0b21befaecde9024590057765c14645
MD Amdadul Islam <58988171+amdadul3036@users.noreply.github.com>
2020-10-09 20:13:04 -0700

    Object

---
+++ Object Oriented Python/object.py
@@ -0,0 +1,17 @@
+class Computer:
+    def config(self):
+        print('Dhrubo Dhruboish Amdadul')
+
+
+comp1 = Computer()
+
+
+# Computer.config(comp1)
+# সাধারণত এভাবে লেখা হয় না।
+
+
+comp1.config()
+
+
+# OUTPUT
+# Dhrubo Dhruboish Amdadul
